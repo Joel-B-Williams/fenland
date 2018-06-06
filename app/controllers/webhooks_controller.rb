dcb01af2c2b68a8a243825c8b84f1b90abed3aa7
joel-b-williams <joel.williams@intercom.io>
2018-06-06 15:54:55 -0500

    add cors header

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,6 +5,7 @@ class WebhooksController < ApplicationController
     def cors_set_access_control_headers
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         headers['Access-Control-Max-Age'] = "1728000"
     end
 
