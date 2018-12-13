cd6962f01150a4b8947fb102718dc4d749964637
Joel Williams <joel.b.williams.is@gmail.com>
2018-12-12 20:50:57 -0600

    many cat choices were unlocked

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,18 +2,38 @@ class WebhooksController < ApplicationController
   protect_from_forgery with: :null_session
 
   def init
-    p "*" * 50
-    p 'You have initialized the app'
     response = {
       canvas: {
         content: {
-          components: [{ 
-            type: "button", 
-            label: "Click ME!!!!", 
-            style: "primary", 
-            id: "url_button", 
-            action: {type: "submit"} 
-          }]
+          components: [
+            { 
+              type: "text", 
+              text: "Please select your favorite part of the cats", 
+              style: "header", 
+              align: "center" 
+            },
+            { 
+              type: "button", 
+              label: "Tiny little toe beans", 
+              style: "primary", 
+              id: "toe_beans", 
+              action: {type: "submit"} 
+            },
+            { 
+              type: "button", 
+              label: "Little snoofer boops", 
+              style: "primary", 
+              id: "snoofer", 
+              action: {type: "submit"} 
+            },
+            { 
+              type: "button", 
+              label: "The softest of bellies", 
+              style: "primary", 
+              id: "bellies", 
+              action: {type: "submit"} 
+            }
+          ]
         }
       }
     }
@@ -23,8 +43,35 @@ class WebhooksController < ApplicationController
   end
   
   def submit
-    p "*" * 50
-    p 'You have submitted the app'
+    catParts = {
+      "toe_beans" => "https://drive.google.com/open?id=1zAysPbk0z4cARfz_TAc_sOHfActQb6EZ",
+      "snoofer" => "https://drive.google.com/open?id=1YaTEIOSK3zFdEZ23BsM7fBmwbqeovvzL",
+      "bellies" => "https://drive.google.com/open?id=1gkUGqER0BxumrOihxDgAY8HjJtC_bEka" 
+    }
+    
+    img_link = catParts[params["component_id"]]
+
+    response = {
+      canvas: {
+        content: {
+          components: [
+            { 
+              type: "text", 
+              text: "You have selected wisely", 
+              style: "header", 
+              align: "center" 
+            },
+            {
+              type: "text",
+              text: img_link,
+              align: "center"
+            }
+          ] 
+        }
+      }
+    }
+    
+    render json: response.to_json
   end
 	
 end
